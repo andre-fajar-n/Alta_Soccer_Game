5efab93db1fbb332130a97bb0581da84116c3b76
andre-fajar-n <afajar@alterra.id>
2020-04-25 21:43:27 +0700

    add player1

--- script.js
+++ script.js
@@ -1,56 +1,104 @@
-const canvas = document.createElement('canvas')
-const context = canvas.getContext('2d')
-canvas.width = 1300
-canvas.height = 550
-document.body.appendChild(canvas)
+// const canvas = document.createElement('canvas')
 
-function loop(){
-    context.fillStyle = '#12FF12'
-    context.fillRect(50, 50, canvas.width, canvas.height)
-    requestAnimationFrame(loop)
-}
+// const context = canvas.getContext('2d')
+// canvas.width = 1300
+// canvas.height = 550
+// document.body.appendChild(canvas)
 
-function ball(){
-    context.fillStyle = '#FFFFFF'
-    context.beginPath()
-    context.arc(canvas.width/2, canvas.height/2, 100, Math.PI*2, false)
-    context.closePath()
-    context.fill()
-}
+// function loop(){
+//     context.fillStyle = '#12FF12'
+//     context.fillRect(50, 50, canvas.width, canvas.height)
+//     requestAnimationFrame(loop)
+// }
 
-function drawRect(x, y, w, h, color){
-    context.fillStyle = color;
-    context.fillRect(x, y, w, h);
-}
+// function ball(){
+//     context.fillStyle = '#FFFFFF'
+//     context.beginPath()
+//     context.arc(canvas.width/2, canvas.height/2, 100, Math.PI*2, false)
+//     context.closePath()
+//     context.fill()
+// }
 
-// User Paddle
-const user = {
-    x : 0, // left side of canvas
-    y : (canvas.height - 100)/2, // -100 the height of paddle
-    width : 10,
-    height : 100,
-    score : 0,
-    color : "WHITE"
-}
+// function drawRect(x, y, w, h, color){
+//     context.fillStyle = color;
+//     context.fillRect(x, y, w, h);
+// }
 
-// COM Paddle
-const com = {
-    x : canvas.width - 10, // - width of paddle
-    y : (canvas.height - 100)/2, // -100 the height of paddle
-    width : 10,
-    height : 100,
-    score : 0,
-    color : "WHITE"
-}
+// // User Paddle
+// const user = {
+//     x : 0, // left side of canvas
+//     y : (canvas.height - 100)/2, // -100 the height of paddle
+//     width : 10,
+//     height : 100,
+//     score : 0,
+//     color : "WHITE"
+// }
 
-function render(){
-    loop()
-    ball()
-    drawRect(user.x, user.y, user.width, user.height, user.color);
+// // COM Paddle
+// const com = {
+//     x : canvas.width - 10, // - width of paddle
+//     y : (canvas.height - 100)/2, // -100 the height of paddle
+//     width : 10,
+//     height : 100,
+//     score : 0,
+//     color : "WHITE"
+// }
+
+// function render(){
+//     loop()
+//     ball()
+//     drawRect(user.x, user.y, user.width, user.height, user.color);
         
-    // draw the COM's paddle
-    drawRect(com.x, com.y, com.width, com.height, com.color);
+//     // draw the COM's paddle
+//     drawRect(com.x, com.y, com.width, com.height, com.color);
 
-}
+// }
 
-let loop = setInterval(render, 500)
+// let loop = setInterval(render, 500)
+
+let dodger = document.getElementById('dodger')
+let player1 = dodger.style
+player1.backgroundColor = '#FF69B4'
+player1.left = '25px'
+player1.bottom = '-250px'
+player1.position = 'relative'
+player1.width = player1.height = '75px'
+
+let left_field_player1 = 240
+let top_field_player1 = -25
+let bottom_field_player1 = -500
+let right_field_player1 = 595
+
+let speed = 5
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 37) {
+        var leftNumbers = player1.left.replace('px', '')
+        var left = parseInt(leftNumbers, 10)
+        left -= speed
+        if (left < left_field_player1) player1.left = '25px'
+        else player1.left = `${left}px`
+    }
+    if (e.which === 39) {
+        var rightNumbers = player1.left.replace('px', '')
+        var right = parseInt(rightNumbers, 10)
+        right += speed
+        if (right > right_field_player1) player1.left = '25px'
+        else player1.left = `${right}px`
+        player1.left = `${right}px`
+    }
+    if (e.which === 38) {
+        var topNumbers = player1.bottom.replace('px', '')
+        var top = parseInt(topNumbers, 10)
+        top += speed
+        if (right > right_field_player1) player1.left = '25px'
+        else player1.left = `${right}px`
+        player1.bottom = `${top + 5}px`
+    }
+    if (e.which === 40) {
+        var bottomNumbers = player1.bottom.replace('px', '')
+        var bottom = parseInt(bottomNumbers, 10)
+        player1.bottom = `${bottom - 5}px`
+    }
+  })
+  
